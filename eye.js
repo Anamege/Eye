9181883d6001e1572c8a718ea4eba5f0937dfc6d
Anamege <87146339+Anamege@users.noreply.github.com>
2021-09-22 22:06:20 -0400

    Update eye.js

--- eye.js
+++ eye.js
@@ -1,4 +1,4 @@
-const balls = document.getElementsByClassName('ball');
+let balls = document.getElementsByClassName('ball');
 
 document.onmousemove = (event) => {
   const x = (event.clientX * 100) / window.innerWidth + '%';
